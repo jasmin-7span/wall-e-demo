e9f5dd6e0710d89289c4ee95e49fd248556f10a3
Bhavin Panchal <bhavin@7span.com>
2024-03-27 12:11:17 +0530

    UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -182,16 +182,35 @@ function App() {
   // }, []);
 
   return (
-    <div className="p-4">
-      <h1 className="font-bold text-xl py-4">React Calendar</h1>
+    <div className="max-w-[992px] mx-auto my-[100px] bg-[#ECEFF1] rounded-3xl border-8 border-black p-5">
+      <div className="flex justify-between pb-5 border-b border-gray-900 mb-5">
+        <div>
+          <h1 className="font-bold text-xl ">Yoat Tour</h1>
+          <div className="flex">
+            <div>
+              <label htmlFor="" className="font-bold text-gray-500">Start Date:</label>
+              <span>11/03/2024</span>
+            </div>
+            <div className="ml-5">
+              <label className="font-bold text-gray-500" htmlFor="">End Date:</label>
+              <span>19/03/2024</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <label className="font-bold text-lg text-gray-500">Yoat Trip:</label>
+          <p><b>Bombay</b> to <b>Goa</b> to <b>Bombay</b></p>
+        </div>
+      </div>
       <div className="flex items-center">
         <LeftArrowIcon className="w-8 h-8 cursor-pointer" />
-        {dates.map((date, index) => {
+        <div className="flex rounded-lg overflow-hidden">
+          {dates.map((date, index) => {
           return (
             <button
               key={index}
               className={`date-card flex flex-col ${
-                date.day === selectDay ? "bg-sky-500" : "bg-purple-500"
+                date.day === selectDay ? "bg-sky-500" : "bg-[#B0BEC5] border-l "
               }`}
               onClick={() => {
                 setLoading(true);
@@ -207,34 +226,77 @@ function App() {
             </button>
           );
         })}
+        </div>
         <RightArrowIcon className="w-8 h-8 cursor-pointer" />
       </div>
 
-      <table className="mt-10 mx-8">
-        <thead>
-          <tr>
-            {hours.map((hour, index) => {
-              return <th key={index}>{hour + 1}:00</th>;
-            })}
-          </tr>
-        </thead>
-
-        {loading ? (
-          <tbody className="relative">
-            <tr className="absolute top-4 flex justify-center w-full">
-              <div className="loader"></div>
-            </tr>
-          </tbody>
-        ) : (
-          <tbody>
-            {/* Sailings */}
+      <div className="overflow-auto border border-gray-900 mt-5 rounded-xl">
+        <table className="w-full ">
+          <thead>
             <tr>
-              <td colSpan={24}>
-                <span className="font-bold text-xl">Sailings</span>
-              </td>
+              {hours.map((hour, index) => {
+                return <th key={index}>{hour + 1}:00</th>;
+              })}
             </tr>
-            {getSplitEvent(eventData[selectDay - 1].sailings).map(
-              (row, index) => {
+          </thead>
+
+          {loading ? (
+            <tbody className="relative">
+              <tr className="absolute top-4 flex justify-center w-full">
+                <div className="loader"></div>
+              </tr>
+            </tbody>
+          ) : (
+            <tbody>
+              {/* Sailings */}
+              <tr>
+                <td colSpan={24}>
+                  <span className="font-bold text-xl">Sailings</span>
+                </td>
+              </tr>
+              {getSplitEvent(eventData[selectDay - 1].sailings).map(
+                (row, index) => {
+                  return (
+                    <tr key={index}>
+                      {row.map((event, index) => {
+                        return (
+                          <React.Fragment key={index}>
+                            {index === 0 ? (
+                              <td colSpan={event.start - 1}></td>
+                            ) : null}
+                            <td
+                              colSpan={event.end - event.start}
+                              className="bg-sky-500"
+                            >
+                              {event.eventName}
+                            </td>
+                            {index !== row.length - 1 ? (
+                              Boolean(row[index + 1].start - event.end) && (
+                                <td colSpan={row[index + 1].start - event.end}>
+                                  {/* {row[index + 1].start - event.end} */}
+                                </td>
+                              )
+                            ) : (
+                              <td colSpan={24 - event.end + 1}>
+                                {/* {24 - event.end + 1} */}
+                              </td>
+                            )}
+                          </React.Fragment>
+                        );
+                      })}
+                    </tr>
+                  );
+                }
+              )}
+
+              {/* Meals */}
+              <tr>
+                <td colSpan={24}>
+                  <span className="font-bold text-xl">Meals</span>
+                </td>
+              </tr>
+              {getSplitEvent(eventData[selectDay - 1].meals).map((row, index) => {
+                console.log("row", row);
                 return (
                   <tr key={index}>
                     {row.map((event, index) => {
@@ -265,88 +327,48 @@ function App() {
                     })}
                   </tr>
                 );
-              }
-            )}
+              })}
 
-            {/* Meals */}
-            <tr>
-              <td colSpan={24}>
-                <span className="font-bold text-xl">Meals</span>
-              </td>
-            </tr>
-            {getSplitEvent(eventData[selectDay - 1].meals).map((row, index) => {
-              console.log("row", row);
-              return (
-                <tr key={index}>
-                  {row.map((event, index) => {
-                    return (
-                      <React.Fragment key={index}>
-                        {index === 0 ? (
-                          <td colSpan={event.start - 1}></td>
-                        ) : null}
-                        <td
-                          colSpan={event.end - event.start}
-                          className="bg-sky-500"
-                        >
-                          {event.eventName}
-                        </td>
-                        {index !== row.length - 1 ? (
-                          Boolean(row[index + 1].start - event.end) && (
-                            <td colSpan={row[index + 1].start - event.end}>
-                              {/* {row[index + 1].start - event.end} */}
-                            </td>
-                          )
-                        ) : (
-                          <td colSpan={24 - event.end + 1}>
-                            {/* {24 - event.end + 1} */}
+              {/* Events */}
+              <tr>
+                <td colSpan={24}>
+                  <span className="font-bold text-xl">Events</span>
+                </td>
+              </tr>
+              {getSplitEvent(eventData[selectDay - 1].events).map(
+                (row, index) => (
+                  <tr key={index}>
+                    {row.map((event, index) => {
+                      return (
+                        <React.Fragment key={index}>
+                          {index === 0 ? (
+                            <td colSpan={event.start - 1}></td>
+                          ) : null}
+                          <td
+                            colSpan={event.end - event.start}
+                            className="bg-sky-500"
+                          >
+                            {event.eventName}
                           </td>
-                        )}
-                      </React.Fragment>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-
-            {/* Events */}
-            <tr>
-              <td colSpan={24}>
-                <span className="font-bold text-xl">Events</span>
-              </td>
-            </tr>
-            {getSplitEvent(eventData[selectDay - 1].events).map(
-              (row, index) => (
-                <tr key={index}>
-                  {row.map((event, index) => {
-                    return (
-                      <React.Fragment key={index}>
-                        {index === 0 ? (
-                          <td colSpan={event.start - 1}></td>
-                        ) : null}
-                        <td
-                          colSpan={event.end - event.start}
-                          className="bg-sky-500"
-                        >
-                          {event.eventName}
-                        </td>
-                        {index !== row.length - 1 ? (
-                          Boolean(row[index + 1].start - event.end) && (
-                            <td colSpan={row[index + 1].start - event.end}>
-                              {/* {row[index + 1].start - event.end} */}
-                            </td>
-                          )
-                        ) : (
-                          <td colSpan={24 - event.end + 1}></td>
-                        )}
-                      </React.Fragment>
-                    );
-                  })}
-                </tr>
-              )
-            )}
-          </tbody>
-        )}
-      </table>
+                          {index !== row.length - 1 ? (
+                            Boolean(row[index + 1].start - event.end) && (
+                              <td colSpan={row[index + 1].start - event.end}>
+                                {/* {row[index + 1].start - event.end} */}
+                              </td>
+                            )
+                          ) : (
+                            <td colSpan={24 - event.end + 1}></td>
+                          )}
+                        </React.Fragment>
+                      );
+                    })}
+                  </tr>
+                )
+              )}
+            </tbody>
+          )}
+        </table>
+      </div>
     </div>
   );
 }
