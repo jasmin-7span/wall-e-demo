d30f34019cde038037e571750e67a58e323e53cb
Bhavin Panchal <bhavin@7span.com>
2024-03-28 14:40:53 +0530

    Yoat changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { LeftArrowIcon } from "./components/icons/LeftArrowIcon";
 import React, { useState } from "react";
 import { useEffect } from "react";
+import SingleDay from "./components/single-day";
+
 import { RightArrowIcon } from "./components/icons/RightArrowIcon";
 
 function App() {
@@ -49,140 +51,94 @@ function App() {
     },
   ];
 
+  const hours = Array.from({ length: 24 }, (_, index) => index);
   const eventData = [
     {
-      // tour: {
-      //   startDate: "11-3-24",
-      //   endDate: "20-3-24",
-      // },
       day: 1,
       date: "11-mar-2024",
       sailings: [
         {
-          start: 4,
-          end: 5,
-          eventName: "Sailing through Bombay Shore",
+          start: '4:30',
+          end: '10:15',
+          name: "Sailing through",
+          color:"red"
+        },
+        //  {
+        //   start: '09:00',
+        //   end: '11:45',
+        //    name: "Sailing dublicate",
+        //   color:"green"
+        // },
+        {
+          start: '12:30',
+          end: '17:00',
+          name: "Sailing through Bombay Shore",
+          color:"orange"
         },
         {
-          start: 9,
-          end: 12,
-          eventName: "Diving in Hind Mahasagar",
+          start: '19:00',
+          end: '23:30',
+          name: "Diving in Hind Mahasagar",
+          color:"pink"
         },
       ],
       meals: [
-        { start: 6, end: 8, eventName: "High Tea" },
-        { start: 12, end: 14, eventName: "Lunch" },
-        { start: 18, end: 20, eventName: "Dinner" },
+        { start: '06:00', end: '08:15', name: "High Tea" },
+        { start: '12:30', end: '14:30', name: "Lunch" },
+        { start: '18:45', end: '20:30', name: "Dinner" },
       ],
       events: [
-        { start: 2, end: 4, eventName: "Surfing" },
-        { start: 3, end: 6, eventName: "Karaoke" },
-        { start: 5, end: 8, eventName: "DJ Night" },
-        { start: 7, end: 12, eventName: "Hello" },
+        { start: '02:30', end: '04:00', name: "Surfing" },
+        { start: '03:00', end: '06:00', name: "Karaoke" },
+        { start: '05:30', end: '08:30', name: "DJ Night" },
+        { start: '07:00', end: '12:30', name: "Hello" },
       ],
     },
-    {
-      // tour: {
-      //   startDate: "11-3-24",
-      //   endDate: "20-3-24",
-      // },
-      // dates: [
-      //   {
-      //     day: 1,
-      //     date: "11-3-2024",
-      //   },
-      //   {
-      //     day: 2,
-      //     date: "12-3-2024",
-      //   },
-      //   {
-      //     day: 3,
-      //     date: "13-3-2024",
-      //   },
-      //   {
-      //     day: 4,
-      //     date: "14-3-2024",
-      //   },
-      //   {
-      //     day: 5,
-      //     date: "15-3-2024",
-      //   },
-      //   {
-      //     day: 6,
-      //     date: "16-3-2024",
-      //   },
-      //   {
-      //     day: 7,
-      //     date: "17-3-2024",
-      //   },
-      // ],
-      day: 2,
-      date: "12-mar-2024",
-      sailings: [
-        {
-          start: 2,
-          end: 5,
-          eventName: "Sailing through Bombay Shore",
-        },
-        {
-          start: 9,
-          end: 12,
-          eventName: "Diving in Hind Mahasagar",
-        },
-      ],
-      meals: [
-        { start: 7, end: 8, eventName: "High Tea" },
-        { start: 15, end: 17, eventName: "Lunch" },
-        { start: 20, end: 22, eventName: "Dinner" },
-      ],
-      events: [
-        { start: 1, end: 3, eventName: "Surfing" },
-        { start: 2, end: 4, eventName: "Karaoke" },
-        { start: 5, end: 8, eventName: "DJ Night" },
-        { start: 6, end: 7, eventName: "Hello" },
-      ],
-    },
+    // {
+    //   day: 2,
+    //   date: "12-mar-2024",
+    //   sailings: [
+    //     {
+    //       start: '04:30',
+    //       end: '06:00',
+    //       name: "Sailing through",
+    //       color:"red"
+    //     },
+    //     //  {
+    //     //   start: '09:00',
+    //     //   end: '11:45',
+    //     //    name: "Sailing dublicate",
+    //     //   color:"green"
+    //     // },
+    //     {
+    //       start: '12:30',
+    //       end: '14:30',
+    //       name: "Sailing through Bombay Shore",
+    //       color:"orange"
+    //     },
+    //     {
+    //       start: '19:00',
+    //       end: '23:00',
+    //       name: "Diving in Hind Mahasagar",
+    //       color:"pink"
+    //     },
+    //   ],
+    //   meals: [
+    //     { start: '06:00', end: '08:15', name: "High Tea" },
+    //     { start: '12:30', end: '14:30', name: "Lunch" },
+    //     { start: '18:45', end: '20:30', name: "Dinner" },
+    //   ],
+    //   events: [
+    //     { start: '02:30', end: '04:00', name: "Surfing" },
+    //     { start: '03:00', end: '06:00', name: "Karaoke" },
+    //     { start: '05:30', end: '08:30', name: "DJ Night" },
+    //     { start: '07:00', end: '12:30', name: "Hello" },
+    //   ],
+    // },
   ];
 
-  function findEvent(array, v) {
-    for (let i = 0; i < array.length; i++) {
-      if (
-        (array[i].start <= v.start && array[i].end > v.start) ||
-        (array[i].start < v.end && array[i].end >= v.end)
-      )
-        return false;
-    }
-
-    return true;
-  }
-
-  function getSplitEvent(events) {
-    return events.reduce((accumulator, currentValue) => {
-      const pushArray = accumulator.find((arr) => findEvent(arr, currentValue));
-      if (pushArray) pushArray.push(currentValue);
-      else accumulator.push([currentValue]);
-
-      return accumulator;
-    }, []);
-  }
-
-  // const getDates = () => {
-  //   const dates = eachDayOfInterval({
-  //     start: new Date(2024, 3, 11),
-  //     end: new Date(2024, 3, 20),
-  //   });
-  //   const formattedDates = dates.map((date) => format(date, "dd-MM-yyyy"));
-  //   setDates(formattedDates);
-  // };
-
-  const hours = Array.from({ length: 24 }, (_, index) => index);
-
-  // useEffect(() => {
-  //   getDates();
-  // }, []);
-
   return (
-    <div className="max-w-[992px] mx-auto my-[100px] bg-[#ECEFF1] rounded-3xl border-8 border-black p-5">
+    <div className="max-w-[1024px] mx-auto my-[100px] bg-[#ECEFF1] rounded-3xl border-8 border-black p-5">
       <div className="flex justify-between pb-5 border-b border-gray-900 mb-5">
         <div>
           <h1 className="font-bold text-xl ">Yoat Tour</h1>
@@ -230,144 +186,21 @@ function App() {
         <RightArrowIcon className="w-8 h-8 cursor-pointer" />
       </div>
 
-      <div className="overflow-auto border border-gray-900 mt-5 rounded-xl">
-        <table className="w-full ">
-          <thead>
-            <tr>
+      <div className="border border-gray-900 mt-5 rounded-xl">
+        <div className="max-w-[1024px] mx-auto bg-gray-200 rounded-xl">
+          <div className="w-full overflow-auto">
+            <div className="flex">
               {hours.map((hour, index) => {
-                return <th key={index}>{hour + 1}:00</th>;
+                return <div key={index} className="py-2 px-4 border-l text-sm flex-[0_0_70px] w-[70px] bg-white">{hour == 0 ? '00:00 ' : hour+':'+'00'}</div>;
               })}
-            </tr>
-          </thead>
-
-          {loading ? (
-            <tbody className="relative">
-              <tr className="absolute top-4 flex justify-center w-full">
-                <div className="loader"></div>
-              </tr>
-            </tbody>
-          ) : (
-            <tbody>
-              {/* Sailings */}
-              <tr>
-                <td colSpan={24}>
-                  <span className="font-bold text-xl">Sailings</span>
-                </td>
-              </tr>
-              {getSplitEvent(eventData[selectDay - 1].sailings).map(
-                (row, index) => {
-                  return (
-                    <tr key={index}>
-                      {row.map((event, index) => {
-                        return (
-                          <React.Fragment key={index}>
-                            {index === 0 ? (
-                              <td colSpan={event.start - 1}></td>
-                            ) : null}
-                            <td
-                              colSpan={event.end - event.start}
-                              className="bg-sky-500"
-                            >
-                              {event.eventName}
-                            </td>
-                            {index !== row.length - 1 ? (
-                              Boolean(row[index + 1].start - event.end) && (
-                                <td colSpan={row[index + 1].start - event.end}>
-                                  {/* {row[index + 1].start - event.end} */}
-                                </td>
-                              )
-                            ) : (
-                              <td colSpan={24 - event.end + 1}>
-                                {/* {24 - event.end + 1} */}
-                              </td>
-                            )}
-                          </React.Fragment>
-                        );
-                      })}
-                    </tr>
-                  );
-                }
-              )}
-
-              {/* Meals */}
-              <tr>
-                <td colSpan={24}>
-                  <span className="font-bold text-xl">Meals</span>
-                </td>
-              </tr>
-              {getSplitEvent(eventData[selectDay - 1].meals).map((row, index) => {
-                console.log("row", row);
-                return (
-                  <tr key={index}>
-                    {row.map((event, index) => {
-                      return (
-                        <React.Fragment key={index}>
-                          {index === 0 ? (
-                            <td colSpan={event.start - 1}></td>
-                          ) : null}
-                          <td
-                            colSpan={event.end - event.start}
-                            className="bg-sky-500"
-                          >
-                            {event.eventName}
-                          </td>
-                          {index !== row.length - 1 ? (
-                            Boolean(row[index + 1].start - event.end) && (
-                              <td colSpan={row[index + 1].start - event.end}>
-                                {/* {row[index + 1].start - event.end} */}
-                              </td>
-                            )
-                          ) : (
-                            <td colSpan={24 - event.end + 1}>
-                              {/* {24 - event.end + 1} */}
-                            </td>
-                          )}
-                        </React.Fragment>
-                      );
-                    })}
-                  </tr>
-                );
+            </div>
+            <div>
+              {eventData.map((events, index) => {
+                return <SingleDay key={index} events={events} />;
               })}
-
-              {/* Events */}
-              <tr>
-                <td colSpan={24}>
-                  <span className="font-bold text-xl">Events</span>
-                </td>
-              </tr>
-              {getSplitEvent(eventData[selectDay - 1].events).map(
-                (row, index) => (
-                  <tr key={index}>
-                    {row.map((event, index) => {
-                      return (
-                        <React.Fragment key={index}>
-                          {index === 0 ? (
-                            <td colSpan={event.start - 1}></td>
-                          ) : null}
-                          <td
-                            colSpan={event.end - event.start}
-                            className="bg-sky-500"
-                          >
-                            {event.eventName}
-                          </td>
-                          {index !== row.length - 1 ? (
-                            Boolean(row[index + 1].start - event.end) && (
-                              <td colSpan={row[index + 1].start - event.end}>
-                                {/* {row[index + 1].start - event.end} */}
-                              </td>
-                            )
-                          ) : (
-                            <td colSpan={24 - event.end + 1}></td>
-                          )}
-                        </React.Fragment>
-                      );
-                    })}
-                  </tr>
-                )
-              )}
-            </tbody>
-          )}
-        </table>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/single-event.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+const SingleEvent = ({ event, index, events,setConflictEvent,conflictEvent,setIsCompleted }) => {
+  const emptyEvent = () => {
+    let width = 0;
+    const eventStartTimeInHors =
+      +event.start.split(":")[0] + +(event.start.split(":")[1] / 60);
+    if (index === 0) {
+      width = eventStartTimeInHors;
+    } else {
+      const minusTime =
+        +events.sailings[index - 1].end.split(":")[0] +
+        +events.sailings[index - 1].end.split(":")[1] / 60;
+      width = eventStartTimeInHors - minusTime;
+    }
+
+    return width * 70 + "px";
+  };
+  const fillEvent = () => {
+    const eventStartTime =
+      +event.start.split(":")[0] + +event.start.split(":")[1] / 60;
+    const eventEndTime =
+      +event.end.split(":")[0] + +event.end.split(":")[1] / 60;
+    if (index !== 0) {
+      const prevEventEndTime =
+        +events.sailings[index - 1].end.split(":")[0] +
+        +events.sailings[index - 1].end.split(":")[1] / 60;
+      if (prevEventEndTime > eventStartTime) {
+          console.log("Hello Maulik");
+        //   setConflictEvent([event])
+      } 
+    }
+      if (index === events.sailings.length) {
+        //   setIsCompleted(false)
+      }
+    return (eventEndTime - eventStartTime) * 70 + "px";
+  };
+  return (
+    <>
+      <div
+        style={{ flexBasis: emptyEvent(), flexGrow: 0, flexShrink: 0 }}
+      ></div>
+      <div
+        style={{
+          background: event.color,
+          flexBasis: fillEvent(),
+          flexGrow: 0,
+          flexShrink: 0,
+        }}
+      >
+        {event.name}
+      </div>
+      
+    </>
+  );
+};
+
+export default React.memo(SingleEvent);
